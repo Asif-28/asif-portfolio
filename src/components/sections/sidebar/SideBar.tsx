e73f020fda56a2d43c53279ea9b92b96e9be10c4
Asif Ali <asif.a.7277@gmail.com>
2023-11-12 16:38:16 +0530

    header ui change

--- src/components/sections/sidebar/SideBar.tsx
+++ src/components/sections/sidebar/SideBar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const SideBar = () => {
   return (
     <section className="section">
-      <div className="h-[calc(100vh-100px)] w-20 border-[2px] rounded-2xl fixed top-[5.2rem]"></div>
+      <div className="h-[calc(100vh-100px)] w-20 border rounded-2xl fixed top-[5.2rem]"></div>
     </section>
   );
 };
