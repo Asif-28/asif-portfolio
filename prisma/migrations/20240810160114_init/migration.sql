af9574a0b8bb1ba2e3d6cf9b61e9000e54318028
Asif Ali <asif.a.7277@gmail.com>
2024-08-11 02:36:56 +0530

    added the compose file and network bridge

--- prisma/migrations/20240810160114_init/migration.sql
+++ prisma/migrations/20240810160114_init/migration.sql
@@ -3,6 +3,7 @@ CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
     "name" TEXT,
+    "description" TEXT,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
---
+++ prisma/migrations/20240810160446_init/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "check" BOOLEAN NOT NULL DEFAULT false;
